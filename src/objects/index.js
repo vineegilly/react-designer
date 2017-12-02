f037f12bc1b07e5033552f0f1f337177a7900dea
Peter Kong <bansuki@gmail.com>
2017-12-01 23:55:50 -0600

    npm install works

--- src/objects/index.js
+++ src/objects/index.js
@@ -3,4 +3,4 @@ export { default as Path } from './Path';
 export { default as Rect } from './Rect';
 export { default as Circle } from './Circle';
 export { default as Text } from './Text';
-export { default as Image } fron './Image';
+export { default as Image } from './Image';
