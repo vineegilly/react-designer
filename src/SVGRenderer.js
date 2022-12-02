9e52188a864fed2bd5ff189d1a4e1ef3a4edffad
shaik chandbasha <shaikchandbasha@scriptbees.com>
2022-12-02 17:18:33 +0530

    keyboard navigation for preview

--- src/SVGRenderer.js
+++ src/SVGRenderer.js
@@ -14,8 +14,13 @@ class SVGRenderer extends Component {
   renderObject(object, index) {
     let {objectRefs, onMouseOver} = this.props;
     let Renderer = this.getObjectComponent(object.type);
+    const onKeyPress = (e) => { 
+      if(e.code ==="Space" || e.code==="Enter"){
+        this.props.clickHandler(object)
+      }
+     }
     return (
-      <Renderer onRender={(ref) => objectRefs[index] = ref} onClick={()=>this.props.clickHandler(object)} className={`${object.type}-${index}`}
+      <Renderer onRender={(ref) => objectRefs[index] = ref} onKeyPress={(e)=>onKeyPress(e)} onClick={()=>this.props.clickHandler(object)} className={`${object.type}-${index}`}
         onMouseOver={onMouseOver.bind(this, index)}
         object={object}  key={index} index={index} />
       );
