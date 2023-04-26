c23a207fa54b948af04c0b8154ebfcd4b6e0719a
vineethkumar.kota <vineegilly@gmail.com>
2023-04-26 21:02:46 +0530

    bug fixes

--- src/panels/Column.js
+++ src/panels/Column.js
@@ -10,22 +10,25 @@ const Column = ({showIf=true, ...props}) => {
 
   const handleOnchange = (e) => {
     let result = e.target.value;
-    if(e.target.value !== '') {
+    if (isNaN(e.target.value) === true) {
       if (props.type === "number") {
         if (isNaN(e.target.value) === false) {
-          result = parseInt(e.target.value);
-          if((props.label === 'width' || props.label === 'height') && result < 5 ){
+          result = parseInt(result);
+          if ((props.label === 'width' || props.label === 'height') && ((result < 5) || result === '' )) {
             result = 5;
           }
-        } else if(props.label === 'width' || props.label === 'height'){
+        } else if (props.label === 'width' || props.label === 'height') {
           result = 5;
-        }else{
+        } else {
           result = 1;
         }
       }
+    }else{
+      result = 5;
     }
     props.onChange(result);
   }
+  
 
   return (
     <div style={[styles.column, props.style]}>
